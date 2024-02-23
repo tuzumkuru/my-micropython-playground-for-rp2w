3357b7f5e05a394f47d62598251ab6ec31b3d85c
Tolga Uzun <tolgauzun@gmail.com>
2024-02-23 13:37:46 -0500

    Add get_mqtt_broker_parameters method to use available parameters Add keepalive to catch connection drops Bugfix: Use correct mqtt reconnection function

--- main_mqtt_enviro_sensor.py
+++ main_mqtt_enviro_sensor.py
@@ -3,7 +3,7 @@ import uasyncio as asyncio
 import ubinascii
 import machine
 from machine import Pin, I2C
-from umqtt.robust import MQTTClient
+from umqtt.simple import MQTTClient
 from bme688 import *
 import config
 from utilities import sync_time
@@ -16,8 +16,24 @@ from logger import log
 MQTT_BROKER = config.MQTT_BROKER
 CLIENT_ID = ubinascii.hexlify(machine.unique_id()).decode('utf-8')
 MQTT_TOPIC = CLIENT_ID + '/' + "BME688"
-MQTT_USER = config.MQTT_USER
-MQTT_PASSWORD = config.MQTT_PASSWORD
+
+
+def get_mqtt_broker_parameters():
+    MQTT_ARGS = {
+    'client_id': ubinascii.hexlify(machine.unique_id()).decode('utf-8'),
+    'server': config.MQTT_BROKER
+    }
+
+    if hasattr(config, 'MQTT_PORT') and config.MQTT_PORT:
+        MQTT_ARGS['port'] = config.MQTT_PORT
+
+    if hasattr(config, 'MQTT_USER') and config.MQTT_USER:
+        MQTT_ARGS['user'] = config.MQTT_USER
+
+    if hasattr(config, 'MQTT_USER') and config.MQTT_PASSWORD:
+        MQTT_ARGS['password'] = config.MQTT_PASSWORD
+
+    return MQTT_ARGS
 
 DATA_SEND_PERIOD = 60
 
@@ -49,7 +65,9 @@ async def task_main():
     bme = BME680_I2C(i2c)
     
     log(f"Connecting to the mqtt broker {MQTT_BROKER}")
-    mqtt_client = MQTTClient(CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
+    
+    mqtt_client = MQTTClient(**get_mqtt_broker_parameters(), keepalive=120)
+
     mqtt_client.connect()
 
     # Time settings
@@ -66,16 +84,18 @@ async def task_main():
             try:
                 bme_data = await get_sensor_data(bme)        
                 bme_data["timestamp"] = time.time()
-                log(f"{current_time - previous_time} seconds passed. Sending data!")
+                log(f"{current_time - previous_time} seconds passed. Sending data to {MQTT_TOPIC}!")
                 mqtt_client.publish(MQTT_TOPIC, json.dumps(bme_data))
+                log(f"Data sent!")
                 previous_time = current_time
             except OSError as e:
                 # Handle connection error
                 log(f"Error occurred while sending data: {e}", file_path="log.log")
                 if not wlan.is_connected:
-                    log("No internet connection.")
-                    wifi = await wlan.connect_async(config.WIFI_SSID,config.WIFI_PASSWORD)    
-                mqtt_client.reconnect()
+                    log("No internet connection. Trying to connect again!")
+                    wifi = await wlan.connect_async(config.WIFI_SSID,config.WIFI_PASSWORD)
+                log(f"Reconnecting to mqtt_client!", file_path="log.log")
+                mqtt_client.connect(False)
             except Exception as e:
                 log(f"Other error occurred while sending data: {e}", file_path="log.log")
 
